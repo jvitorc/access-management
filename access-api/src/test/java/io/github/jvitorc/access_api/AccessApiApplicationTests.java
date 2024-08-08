2d05a1cb35251e9b28bb958d47deceb524131071
joaov <joao.vitor.brasill@gmail.com>
2024-08-08 00:03:48 -0300

    [code] created the spring project - access-api

---
+++ access-api/src/test/java/io/github/jvitorc/access_api/AccessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jvitorc.access_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
